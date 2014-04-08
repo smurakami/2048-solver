9ee50277824ba903a530130e51bde6f0f064dae2
s_murakami <s.murakami@conol.jp>
2014-04-08 11:43:23 +0900

    Created sover.js

--- 2048-solver.js
+++ 2048-solver.js
@@ -1,18 +1,22 @@
 /* ============================= *
+ * solver.js
+ * ============================= */
+
+
+/* ============================= *
  * readmap.js
  * ============================= */
 
-var Map = function(){
-  for (var i = 0; i < 4; i++) {
-    this[i] = [0, 0, 0, 0];
-  }
-};
-
-Map.prototype = new Array();
-
-var getTileMap = function () {
+var readMap = function () {
   // Initialize
-  var map = new Map();
+  var MAP_SIZE = 4;
+  var map = new Array(MAP_SIZE);
+  for (var i = 0; i < MAP_SIZE; i++) {
+    map[i] = Array(4);
+    for (var j = 0; j < MAP_SIZE; j++){
+      map[i][j] = 0;
+    }
+  }
   // Get tiles from DOM tree
   var tiles = [];
   var tileContainer = document.querySelector('.tile-container');
@@ -46,8 +50,6 @@ var getTileMap = function () {
     map[tile.y][tile.x] = tile.num;
   }
 
-  printMap(map);
-
   return map;
 };
 
@@ -65,3 +67,45 @@ var printMap = function(map) {
 /* ============================= *
  * controller.js
  * ============================= */
+
+var Controller = function(){};
+Controller.prototype.keydown = function(k) {
+  var body = document.querySelector('body');
+  var oEvent = document.createEvent('KeyboardEvent');
+  // Chromium Hack
+  Object.defineProperty(oEvent, 'keyCode', {
+              get : function() {return this.keyCodeVal; }
+  });
+  Object.defineProperty(oEvent, 'which', {
+              get : function() {return this.keyCodeVal; }
+  });
+  modkeys = ['shiftKey', 'metaKey', 'ctrlKey', 'altKey'];
+  for (var i = 0, l = modkeys.length; i < l; i++){
+    Object.defineProperty(oEvent, modkeys[i], {
+      get : function() {return false; }
+    });
+  }
+  if (oEvent.initKeyboardEvent) {
+      oEvent.initKeyboardEvent("keydown", true, true, document.defaultView, false, false, false, false, k, k);
+  } else {
+      oEvent.initKeyEvent("keydown", true, true, document.defaultView, false, false, false, false, k, 0);
+  }
+  oEvent.keyCodeVal = k;
+  if (oEvent.keyCode !== k) {
+      alert("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ")");
+  }
+  body.dispatchEvent(oEvent);
+}
+
+Controller.prototype.up = function(){
+  this.keydown(38);
+};
+Controller.prototype.right = function(){
+  this.keydown(39);
+};
+Controller.prototype.down = function(){
+  this.keydown(40);
+}
+Controller.prototype.left = function(){
+  this.keydown(37);
+};
--- readmap.js
+++ readmap.js
@@ -1,14 +1,13 @@
-var Map = function(){
-  for (var i = 0; i < 4; i++) {
-    this[i] = [0, 0, 0, 0];
-  }
-};
-
-Map.prototype = new Array();
-
-var getTileMap = function () {
+var readMap = function () {
   // Initialize
-  var map = new Map();
+  var MAP_SIZE = 4;
+  var map = new Array(MAP_SIZE);
+  for (var i = 0; i < MAP_SIZE; i++) {
+    map[i] = Array(4);
+    for (var j = 0; j < MAP_SIZE; j++){
+      map[i][j] = 0;
+    }
+  }
   // Get tiles from DOM tree
   var tiles = [];
   var tileContainer = document.querySelector('.tile-container');
@@ -41,8 +40,6 @@ var getTileMap = function () {
     var tile = tiles[i];
     map[tile.y][tile.x] = tile.num;
   }
-
-  printMap(map);
 
   return map;
 };
